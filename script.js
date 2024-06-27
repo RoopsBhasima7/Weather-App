4e6fefb0c4aa11e67dd26affe189720ce48598ad
roopsbhasima7 <rupeshbhasima@gmail.com>
2024-06-27 15:32:29 +0545

    js file updated

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "841a2cef43ab3cfbbbe5385ca5e2be2a"; //API KEY for OpenWeatherMap API
+const API_KEY = "841a2cef43ab3cfbbbe5385ca5e2be2a"; // API key for OpenWeatherMap API
+
 const createWeatherCard = (weatherItem) => {
     return `<li class="card">
-                <h3>(${weatherItem.dt_text.split("")[0]})</h3>
+                <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
                 <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
                 <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
-                <h4>Wind: ${(weatherItem.wind.speed)} M/S</h4>
-                <h4>Humidity: ${(weatherItem.main.humidity)}%</h4>
+                <h4>Wind: ${weatherItem.wind.speed} M/S</h4>
+                <h4>Humidity: ${weatherItem.main.humidity}%</h4>
             </li>`;
 }
 
@@ -17,31 +18,33 @@ const getWeatherDetails = (cityName, lat, lon) => {
     const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 
     fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
-        // Filter the forecast to get only one forecast per day
+        console.log(data);
+
+        //Filter the forecasts to get only one forecast per day
         const uniqueForecastDays = [];
         const fiveDaysForecast = data.list.filter(forecast => {
-            const forecastDate = new Date(forecast.dt_text).getDate();
+            const forecastDate = new Date(forecast.dt_txt).getDate();
             if(!uniqueForecastDays.includes(forecastDate)) {
                 return uniqueForecastDays.push(forecastDate);
             }
         });
 
-        //Clearing previous weather data
+        // Clearing previous weather data
         cityInput.value = "";
         weatherCardsDiv.innerHTML = "";
 
-        console.log(fiveDaysForecast);
-        fiveDaysForecast.array.forEach(weatherItem => {
+       console.log(fiveDaysForecast);
+       fiveDaysForecast.forEach(weatherItem => {
             weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-        });
+       });
     }).catch(() => {
-        alert("An error occurred while fetching the weather forecast!");
+        alert("An error occurred while fetching the weather forecast!")
     });
 }
 
 const getCityCoordinates = () => {
-    const cityName = cityInput.value.trim(); // get user entered city name and remove extra spaces
-    if(!cityName) return; // Return if cityName is empty
+    const cityName = cityInput.value.trim(); // Get user entered city name and remove extra spaces
+    if(!cityName) return;
     const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
     // Get entered city coordinates (latitude, longitude, and name) from the API response
@@ -50,7 +53,8 @@ const getCityCoordinates = () => {
         const { name, lat, lon } = data[0];
         getWeatherDetails(name, lat, lon);
     }).catch(() => {
-        alert("An error occurred while fetching the coordinates!");
+        alert("An error occurred while fetching the coordinates!")
     });
 }
+
 searchButton.addEventListener("click", getCityCoordinates);
